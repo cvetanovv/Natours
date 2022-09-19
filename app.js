369be4ffc5b263d9c2ea00643623bace6df1a247
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-09-19 20:29:09 +0300

    Create POST request to tours API

--- app.js
+++ app.js
@@ -3,16 +3,7 @@ const express = require("express");
 
 const app = express();
 
-// app.get("/", (req, res) => {
-//     res.status(200).json({
-//         message: "Hello from the server!",
-//         app: "natours",
-//     });
-// });
-
-// app.post("/", (req, res) => {
-//     res.send("You can POST to this endpoint")
-// })
+app.use(express.json());
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -26,6 +17,26 @@ app.get("/api/v1/tours", (req, res) => {
             tours: tours,
         },
     });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        (err) => {
+            res.status(201).json({
+                status: "success",
+                data: {
+                    tour: newTour,
+                },
+            });
+        }
+    );
 });
 
 const port = 3000;

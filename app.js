14a6537362eed4fb9ae65888de494c124f4c0f1d
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-09-19 19:58:06 +0300

    Create GET request to tours API

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+// app.get("/", (req, res) => {
+//     res.status(200).json({
+//         message: "Hello from the server!",
+//         app: "natours",
+//     });
+// });
+
+// app.post("/", (req, res) => {
+//     res.send("You can POST to this endpoint")
+// })
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
     res.status(200).json({
-        message: "Hello from the server!",
-        app: "natours",
+        status: "success",
+        results: tours.length,
+        data: {
+            tours: tours,
+        },
     });
 });
 
-app.post("/", (req, res) => {
-    res.send("You can POST to this endpoint")
-})
-
 const port = 3000;
 app.listen(3000, () => {
-    console.log(`App running on poert ${port}...`);
+    console.log(`App running on port ${port}...`);
 });

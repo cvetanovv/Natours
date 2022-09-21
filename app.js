634860b686400593270e099a6987f3e2b2e85070
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-09-21 19:45:34 +0300

    Handling DELETE Requests

--- app.js
+++ app.js
@@ -75,6 +75,20 @@ app.patch("/api/v1/tours/:id", (req, res) => {
     })
 })
 
+app.delete("/api/v1/tours/:id", (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: "Fail",
+            message: "Invalid ID",
+        });
+    }
+
+    res.status(204).json({
+        status: "Success",
+        data: null
+    })
+})
+
 const port = 3000;
 app.listen(3000, () => {
     console.log(`App running on port ${port}...`);

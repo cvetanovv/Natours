d48afbff3ff2eb65707ba6aed11ce57d03a0b7c8
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-09-25 20:24:19 +0300

    Create separate routes

--- app.js
+++ app.js
@@ -135,16 +135,22 @@ const deleteUser = (req, res) => {
 }
 
 // Routes
-app.route("/api/v1/tours").get(getAllTours).post(createTour);
+const tourRouter = express.Router()
+const userRouter = express.Router()
 
-app.route("/api/v1/tours/:id")
+app.use("/api/v1/tours" , tourRouter)
+app.use("/api/v1/users" , userRouter)
+
+tourRouter.route("/").get(getAllTours).post(createTour);
+
+tourRouter.route("/:id")
     .get(getTour)
     .patch(updateTour)
     .delete(deleteTour);
 
-app.route("/api/v1/users").get(getAllUsers).post(createUser);
+userRouter.route("/").get(getAllUsers).post(createUser);
 
-app.route("/api/v1/users/:id")
+userRouter.route("/:id")
     .get(getUser)
     .patch(updateUser)
     .delete(deleteUser);

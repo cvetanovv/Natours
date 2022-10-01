89bb3623b2e417fdf4ea274037d92aafa92da9ec
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-10-01 16:39:11 +0300

    Add Controllers

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,50 +1,18 @@
-const express = require('express')
+const express = require("express");
 
+const userController = require("./../controllers/userController");
 
-const getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: "This route is not yet defined!"
-    })
-}
+const router = express.Router();
 
-const createUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: "This route is not yet defined!"
-    })
-}
+router
+    .route("/")
+    .get(userController.getAllUsers)
+    .post(userController.createUser);
 
-const getUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: "This route is not yet defined!"
-    })
-}
+router
+    .route("/:id")
+    .get(userController.getUser)
+    .patch(userController.updateUser)
+    .delete(userController.deleteUser);
 
-const updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: "This route is not yet defined!"
-    })
-}
-
-const deleteUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: "This route is not yet defined!"
-    })
-}
-
-const router = express.Router()
-
-router.route("/")
-    .get(getAllUsers)
-    .post(createUser);
-
-router.route("/:id")
-    .get(getUser)
-    .patch(updateUser)
-    .delete(deleteUser);
-
-module.exports = router
+module.exports = router;

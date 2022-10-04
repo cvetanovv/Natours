3b05efa92e80cf27ab54cd2d688ea62d446130a3
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-10-04 17:06:30 +0300

    Add Environment Variables

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
 //Middlewares
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -18,7 +21,7 @@ app.use((req, res, next) => {
 
 // Routes
 
-app.use("/api/v1/tours" , tourRouter)
-app.use("/api/v1/users" , userRouter)
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
 module.exports = app;

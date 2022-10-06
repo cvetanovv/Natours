4b1bbee5b6b1904b6cef9dabff5d11655f175c60
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-10-06 19:14:27 +0300

    Create Tour Model

--- server.js
+++ server.js
@@ -9,11 +9,30 @@ const DB = process.env.DATABASE.replace(
     process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-}).then(con => console.log("DB connections successful!"));
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then((con) => console.log("DB connections successful!"));
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "A tour must have a name"],
+        unique: true,
+    },
+    rating: {
+        type: Number,
+        default: 4.5,
+    },
+    price: {
+        type: Number,
+        required: [true, "A tour must have a price"],
+    },
+});
+const Tour = mongoose.model("Tour", tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(3000, () => {

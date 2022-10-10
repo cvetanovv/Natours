dfc76a0e5f1d68dc060b7662151d979cedf73edc
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-10-10 18:21:25 +0300

    Implement Delete tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -82,9 +82,18 @@ exports.updateTour = async (req, res) => {
         }
 };
 
-exports.deleteTour = (req, res) => {
-    res.status(204).json({
-        status: "Success",
-        data: null,
-    });
+exports.deleteTour = async (req, res) => {
+    try{
+        await Tour.findByIdAndDelete(req.params.id)
+        res.status(204).json({
+            status: "Success",
+            data: null
+        });
+    } catch (err) {
+        res.status(400).json({ 
+            status: "Fail",
+            message: err,
+        });
+    }
+
 };
